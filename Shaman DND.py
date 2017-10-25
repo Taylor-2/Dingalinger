553ff40d245cc1bb420dd93dd3ad444fcb1222a2
Dexhubb <hubbade1@gmail.com>
2017-10-25 12:04:15 -0400

    worked on it

--- Shaman DND.py
+++ Shaman DND.py
@@ -5,7 +5,7 @@ from random import *
 import tkinter.messagebox as msg
 
 root=Tk()
-
+#Initialization variables
 dieroll=[]
 
 def roll(event=None):
@@ -25,20 +25,31 @@ def roll(event=None):
 	if (numdies.get() != 1):
 		dieroll += "\nTotal: " + str(total)
 	rollresult.set(dieroll)
-
 def reset_roll(event=None):
 	dietype.set("Pick")
 	numdies.set(1)
 	rollresult.set("Result")
+def Addhp():
+	currenthp=currenthp.get()
+	currenthp=int(currenthp)
+	currenthp += 1
+	currenthp=str(currenthp)
+	currenthp.set(currenthp)
+def Subtracthp():
+	currenthp.set(str(int(currenthp.get() -1)))
 
+#######################################################################
+#Temporary Variables
 firstname="Beppo"
 lastname="McLastname"
 race="Gnome"
 classvar="Wizard"
+maxhp="8"
+currenthp=4
+temp=45678
+########################################################################
 
-maxhp=str(8)
-currenthp=str(4)
-
+#Initializing variable for Tkinter
 numdies=IntVar()
 numdies.set(1)
 dietype=StringVar()
@@ -46,8 +57,12 @@ dietype.set("Pick")
 rollresult=StringVar()
 rollresult.set("Result")
 experiencepoints=IntVar()
-experiencepoints.set(788)
+experiencepoints.set(temp)
+currenthp=StringVar()
+currenthp.set("8")
 
+#########################################################################
+#Creating Tkinter modules
 #Name + Class + Race
 name=Label(root,text=firstname + " " + lastname)
 info=Label(root,text="The " + classvar + " " + race)
@@ -62,14 +77,33 @@ numberofdies.bind("<Delete>", reset_roll)
 
 #Health
 maxhplabel=Label(root,text="Max HP: " + maxhp)
-currenthplabel=Label(root,text="Current HP: " + currenthp)
+currenthplabel=Label(root,text="Current HP: " + str(currenthp.get()))
+addhpbutton=Button(root,text="+",command=Addhp)
+subtracthpbutton=Button(root,text="-",command=Subtracthp)
 
 #Experience
 experiencelabel=Label(root,text="Experience:")
 experiencefield=Entry(root,textvar=experiencepoints,width=8)
 levelupbutton=Button(root,text="Level Up")
 
-name.grid(row=0,columnspan=2,column=0,sticky=W,padx=20)
+#Inventory
+'''scrollbar = Scrollbar(root)
+scrollbar.grid(column=8,rowspan=2,row=0)
+
+ctr =0
+while(ctr < 50):
+	inventory.append(ctr)
+	ctr += 1
+
+inventory = Listbox(root)
+inventory.grid(column=7,rowspan=2,row=0)
+
+inventory.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=inventory.yview)'''
+
+###################################################################################################
+#Display Tkinter modules
+'''name.grid(row=0,columnspan=2,column=0,sticky=W,padx=20)
 info.grid(row=1,columnspan=2,column=0,sticky=W,padx=20)
 
 numberofdies.grid(row=0,column=2)
@@ -82,7 +116,12 @@ currenthplabel.grid(row=1,column=4)
 
 experiencelabel.grid(row=0,columnspan=2,column=5)
 experiencefield.grid(row=1,columnspan=2,column=6)
-#levelupbutton.pack()
+#levelupbutton.pack()'''
+
+maxhplabel.pack()
+currenthplabel.pack()
+addhpbutton.pack()
+subtracthpbutton.pack()
 
 
 mainloop()
